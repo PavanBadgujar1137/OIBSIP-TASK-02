fd70ba362c58d5bc0dc846e689e980b32bce6538
PavanBadgujar1137 <pavanabad01@gmail.com>
2023-02-21 10:02:56 -0800

    Task Completed

---
+++ main.js
@@ -0,0 +1,12 @@
+var typeData = new Typed(".role", {
+  strings: [
+    "Fronted Developer",
+    "Web Developer",
+    "Coder",
+    "Problem Solver (DSA)",
+  ],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1000,
+});
